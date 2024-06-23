477c54f503e2b5a81e4deed5d16932851ed324ed
Peter The Great <ptg@Peters-MacBook-Air.local>
2024-06-23 13:22:39 -0700

    added conts for tests

--- src/FibonacciTest.java
+++ src/FibonacciTest.java
@@ -4,27 +4,30 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FibonacciTest {
 
+    final static int FIBONACCI_INDEX = 6;
+    final static int FIBONACCI_EXPECTED_VALUE = 8;
+
     @Test
     public void findSequence() {
-        final var test = Fibonacci.findSequence(6);
-        assertEquals(8, test);
+        final var test = Fibonacci.findSequence(FIBONACCI_INDEX);
+        assertEquals(FIBONACCI_EXPECTED_VALUE, test);
     }
 
     @Test
     public void findSequenceMemo() {
-        final var test = Fibonacci.findSequenceMemo(6);
-        assertEquals(8, test);
+        final var test = Fibonacci.findSequenceMemo(FIBONACCI_INDEX);
+        assertEquals(FIBONACCI_EXPECTED_VALUE, test);
     }
 
     @Test
     public void findSequenceIterative() {
-        final var test = Fibonacci.findSequenceIterative(6);
-        assertEquals(8, test);
+        final var test = Fibonacci.findSequenceIterative(FIBONACCI_INDEX);
+        assertEquals(FIBONACCI_EXPECTED_VALUE, test);
     }
 
     @Test
     public void findSequenceStream() {
-        final var test = Fibonacci.findSequenceStream(6);
-        assertEquals(8, test);
+        final var test = Fibonacci.findSequenceStream(FIBONACCI_INDEX);
+        assertEquals(FIBONACCI_EXPECTED_VALUE, test);
     }
 }
